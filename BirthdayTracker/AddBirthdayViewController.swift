4c53ed07d158c25a06979610b30593ae410c4401
Margarita Novokh <r28611@gmail.com>
2021-01-17 13:09:50 +0100

    add AddBirthdayViewControllerDelegate add logo

--- BirthdayTracker/AddBirthdayViewController.swift
+++ BirthdayTracker/AddBirthdayViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
+protocol AddBirthdayViewControllerDelegate {
+    func addBirthdayViewController(_ addBirthdayViewController: AddBirthdayViewController, didAddBirthday birthday: Birthday)
+}
+
 class AddBirthdayViewController: UIViewController {
     
     @IBOutlet var firstNameTextField: UITextField!
     @IBOutlet var lastNameTextField: UITextField!
     @IBOutlet var birthdatePicker: UIDatePicker!
     
-    //TO DO: add delegate
+    
+    var delegate: AddBirthdayViewControllerDelegate?
         
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,17 +29,19 @@ class AddBirthdayViewController: UIViewController {
 
     @IBAction func saveTapped(_ sender: UIBarButtonItem) {
         print("Нажата кнопка сохранения.")
-        let firstName=firstNameTextField.text ?? ""
-        let lastName=lastNameTextField.text ?? ""
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
         let birthdate = birthdatePicker.date
         let newBirthday = Birthday(firstName: firstName, lastName: lastName, birthdate: birthdate)
-        print("Меня зовут \(newBirthday.firstName) \(newBirthday.lastName) и др у меня \(newBirthday.birthdate).")
+        delegate?.addBirthdayViewController(self, didAddBirthday: newBirthday)
+        dismiss(animated: true, completion: nil)
     }
     
     
     @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
+    
     
 }
 
--- BirthdayTracker/BirthdaysTableTableViewController.swift
+++ BirthdayTracker/BirthdaysTableTableViewController.swift
@@ -7,11 +7,6 @@
 
 import UIKit
 
-
-protocol AddBirthdayViewControllerDelegate {
-    func addBirthdayViewController (_ addBirthdayViewController: AddBirthdayViewController, didAddBirthday birthday: Birthday)
-}
-
 class BirthdaysTableTableViewController: UITableViewController, AddBirthdayViewControllerDelegate {
 
     var birthdays = [Birthday]()
@@ -83,15 +78,15 @@ class BirthdaysTableTableViewController: UITableViewController, AddBirthdayViewC
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let navigationController = segue.destination as! UINavigationController
+        let addBirthdayViewController = navigationController.topViewController as! AddBirthdayViewController
+        addBirthdayViewController.delegate = self
     }
-    */
+    
     
     // MARK: - AddBirthdayViewControllerDelegate
     func addBirthdayViewController(_ addBirthdayViewController: AddBirthdayViewController, didAddBirthday birthday: Birthday) {
